c115c987ae227c5cb9edf23c93f7e15e47051427
Lucas Iván <lucasmenchi@hotmail.com.ar>
2017-06-24 19:23:52 -0300

    #Resumen de batalla seleccionada obtenido desde Wikipedia en español (buscar por títulos en español y ojo con las tildes)

--- public/components/battle-list.component.js
+++ public/components/battle-list.component.js
@@ -11,8 +11,8 @@ function battleListController($http, $scope, $mdBottomSheet, $rootScope){
     $scope.showBattleList = function() {
       $scope.alert = '';
       $mdBottomSheet.show({
-        templateUrl: '../battle-list.html',
-        controller: BattleListCtrl
+        templateUrl:'../wiki-summary.component.html', // '../battle-list.html',
+        controller: wikiController // BattleListCtrl
       })/*.then(function(clickedItem) {
         $scope.alert = clickedItem['name'] + ' clicked!';
       }).catch(function(error) {
@@ -20,6 +20,21 @@ function battleListController($http, $scope, $mdBottomSheet, $rootScope){
       })*/;
     };
 
+    function wikiController($http, $rootScope, $scope){
+    //Probamos Api de Wikipedia+
+      $http.jsonp('http://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles='+$rootScope.selectedBattle.name+'&callback=JSON_CALLBACK').success(function(response) {
+          var nropagina=Object.keys(response.query.pages)[0];
+          console.log(nropagina);
+          console.log(response.query.pages[nropagina]);
+          $scope.datos=response.query.pages[nropagina].extract;
+          $scope.datos=$scope.datos.replace(/<[^>]*>?/g, '');
+      });
+
+      $scope.close = function() {
+        $mdBottomSheet.hide();
+      };
+    }
+  
     function BattleListCtrl($http, $scope, $mdBottomSheet) {
       $scope.items = [];
       
